2a104f9ada95fabad3299cf9507fbc86094a4978
Raccoon96 <48393354+Raccoon96@users.noreply.github.com>
2021-10-10 13:13:30 +0200

    Add files via upload
    zad 7/8

---
+++ Main.java
@@ -0,0 +1,58 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //zad 7
+        System.out.println("Podaj liczbe: ");
+        Scanner in = new Scanner(System.in);
+        double x = in.nextDouble();
+        System.out.println(doKwadratu(x));
+
+        //zad 8 (testowe do sprawdzenia poprawnosci funkcji czyTrojkat - liczby wybierane przez uzytkownika)
+        System.out.println("Podaj trzy liczby: ");
+        Scanner in2 = new Scanner(System.in);
+        double a = in2.nextDouble();
+        double b = in2.nextDouble();
+        double c = in2.nextDouble();
+        System.out.println(czyTrojkat(a, b, c));
+
+        //zad 8 (losowane z przedzialu podanego przez uzytkownika uzytkownika)
+        System.out.println("Podaj dwie liczby: ");
+        Scanner in3 = new Scanner(System.in);
+        int first = in3.nextInt();
+        int last = in3.nextInt();
+        Random rand = new Random();
+        a = rand.nextInt(last-first+1)+first;
+        b = rand.nextInt(last-first+1)+first;
+        c = rand.nextInt(last-first+1)+first;
+        int f = rand.nextInt((last-first+1)+first);
+        System.out.println(a + " " + b + " " + c + " " + f);
+        System.out.println(czyTrojkat(a, b, c));
+    }
+
+    public static double doKwadratu(double a) {
+        return a*a;
+    }
+
+    public static boolean czyTrojkat(double a, double b, double c) {
+        double max = c;
+        if(a<1 || b<1 || c<1)
+            return false;
+        else if(a>b && a>c){
+            max = a;
+            a = c;
+        }
+        else if(b>c) {
+            max = b;
+            b = c;
+        }
+        if(a*a+b*b==max*max)
+            return true;
+        else
+            return false;
+    }
+}
